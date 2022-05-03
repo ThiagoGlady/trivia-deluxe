3da0fc663ef7c112c1370aa1cd7274efaaa42c44
ThiagoGlady <thiagoglady@gmail.com>
2022-05-03 10:02:44 -0300

    tela de ranking com o botão feito

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Ranking() {
   return (
-    <h3 data-testid="ranking-title">
-      Tela de ranking
-    </h3>
+    <>
+      <h3 data-testid="ranking-title">
+        Tela de ranking
+      </h3>
+      <Link data-testid="btn-go-home" to="/">Play Again</Link>
+    </>
   );
 }
 
